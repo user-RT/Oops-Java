b427e2e2f906fd764ea6469175714fc34c31c37a
khoi <timsinaravi23@gmail.com>
2024-08-16 15:20:35 +0545

    first oops code

---
+++ src/class1.java
@@ -0,0 +1,27 @@
+package src;//Create a class named Person with attributes for firstName and lastName.
+
+class person {
+    int id;
+    String name;
+
+    public void Employee(){
+        System.out.println("Id is: " + id);
+        System.out.println("Name is: " + name);
+    }
+}
+public class class1{
+
+    public static void main(String[] args) {
+
+        person num = new person();
+
+
+        num.id = 1;
+        num.name = "ravi";
+
+        num.Employee();
+
+    }
+
+
+}
